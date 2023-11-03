ee61834709e3161095267f19ca73deb88753d651
Mohitchvk <mohit.chvk@gmail.com>
2023-11-03 16:30:48 -0400

    Update artistic streamlit

--- Part_1/streamlit_visual_search_artistic_style.py
+++ Part_1/streamlit_visual_search_artistic_style.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 import glob
 
 # D:\Projects\ADM Assg 3\Assignment-3--Team-5\Part_1\streamlit_visual_search_artistic_style.py
+model = tf.keras.models.load_model("visual_search_similarity.keras")
 
 with open('image_style_embeddings.pickle', 'rb') as f:
     image_style_embeddings = pickle.load(f)
@@ -65,3 +66,31 @@ def search_by_style(image_style_embeddings, images, reference_image, max_results
     for k,v in image_style_embeddings.items():
         d = sc.spatial.distance.cosine(v0, v)
         distances[k] = d
+
+    sorted_neighbors = sorted(distances.items(), key=lambda x: x[1], reverse=False)
+    
+    f, ax = plt.subplots(1, max_results, figsize=(16, 8))
+    for i, img in enumerate(sorted_neighbors[:max_results]):
+        ax[i].imshow(images[img[0]])
+        ax[i].set_axis_off()
+
+    plt.show()
+    
+
+# images mostly match the reference style, although not perfectly
+search_by_style(image_style_embeddings, images, 's_impressionist-02.jpg')
+plt.savefig('style_plot.png')
+st.image('style_plot.png')
+# images mostly match the reference style, although not perfectly
+# uploaded_image = st.file_uploader("Upload an Image", type=['jpg', 'png', 'jpeg'])
+# st.write(uploaded_image)
+
+# if uploaded_image is not None:
+#     try:
+#       search_by_style(image_style_embeddings, images, uploaded_image)
+#       plt.savefig('search_by_style.png')
+#       st.image('search_by_style.png')
+#     except Exception as e:
+#       st.error(f'Failed to load or process the image: {str(e)}')
+
+
